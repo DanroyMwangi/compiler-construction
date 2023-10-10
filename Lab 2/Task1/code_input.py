5b637b156cd53ec620a84a2e0c7ef27c2909f65f
Trevor-Muthoka <84325467+Trevor-Muthoka@users.noreply.github.com>
2023-10-10 18:41:51 +0300

    Create code_input.py

---
+++ Lab 2/Task1/code_input.py
@@ -0,0 +1,12 @@
+# This script prints out video game quotes.
+
+def videogame_quote_1():
+    print("It's dangerous to go alone! Take this.")
+    # - The Legend of Zelda
+
+def videogame_quote_2():
+    print("War. War never changes.")
+    # - Fallout
+
+videogame_quote_1()
+videogame_quote_2()
